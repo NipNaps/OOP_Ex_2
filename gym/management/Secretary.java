b5385875bca825d4fc8da7194562ea4f0db172bd
NipNaps <omri.besan@gmail.com>
2024-12-23 20:51:22 +0200

    Omri

--- gym/management/Secretary.java
+++ gym/management/Secretary.java
@@ -1,9 +1,10 @@
 package gym.management;
+
 import gym.Exception.ClientNotRegisteredException;
 import gym.Exception.DuplicateClientException;
 import gym.Exception.InstructorNotQualifiedException;
 import gym.Exception.InvalidAgeException;
-import  gym.customers.Client;
+import gym.customers.Client;
 import gym.customers.Person;
 import gym.management.Sessions.Session;
 import gym.management.Sessions.ForumType;
@@ -12,63 +13,110 @@ import gym.management.Sessions.SessionType;
 import java.util.ArrayList;
 import java.util.List;
 
-    public class Secretary extends Person {
-        private double salary;
-        private List<String> actionHistroy;
-        private List<Client> clients;
-        private List<Instructor> instructors;
-        private List<Session> sessions;
+public class Secretary extends Person {
+    private double salary;
+    private List<String> actionHistroy;
+    private List<Client> clients;
+    private List<Instructor> instructors;
+    private List<Session> sessions;
 
-        public Secretary(Person person, double salary) {
-            super(person.getName(), person.getBalance(), person.getGender(), person.getBirthdate());
-            this.salary = salary;
-            this.actionHistroy = new ArrayList<>();
-            this.clients = new ArrayList<>();
-            this.instructors = new ArrayList<>();
-            this.sessions = new ArrayList<>();
-        }
-        public Client registerClient(Person person) throws InvalidAgeException, DuplicateClientException {
-            if (person.getAge() < 18) {
-                throw new InvalidAgeException("Error: Client age must be at least 18 years old");
-            }
-            for (Client client : clients) {
-                if (client.getName().equals(person.getName())) {
-                    throw new DuplicateClientException("Error: Client is already registered");
-                }
-            }
-            Client client = new Client(person.getName(), person.getBalance(), person.getGender(), person.getBirthdate());
-            clients.add(client);
-            logAction("Registered new client: " + client.getName());
-            return client;
+    public Secretary(Person person, double salary) {
+        super(person.getName(), person.getBalance(), person.getGender(), person.getBirthdate());
+        this.salary = salary;
+        this.actionHistroy = new ArrayList<>();
+        this.clients = new ArrayList<>();
+        this.instructors = new ArrayList<>();
+        this.sessions = new ArrayList<>();
+    }
 
+    public Client registerClient(Person person) throws InvalidAgeException, DuplicateClientException {
+        if (person.getAge() < 18) {
+            throw new InvalidAgeException("Error: Client age must be at least 18 years old");
         }
-        public void unregisterClient(Client client) throws ClientNotRegisteredException {
-            if (!clients.contains(client)) {
-                throw new ClientNotRegisteredException("Error: Client is not registered");
+        for (Client client : clients) {
+            if (client.getName().equals(person.getName())) {
+                throw new DuplicateClientException("Error: Client is already registered");
             }
-            clients.remove(client);
-            logAction("Unregistered client: " + client.getName());
         }
-        public Session addSession(SessionType sessionType, String date, ForumType forumType, Instructor instructor)
-        throws InstructorNotQualifiedException {
+        Client client = new Client(person.getName(), person.getBalance(), person.getGender(), person.getBirthdate());
+        clients.add(client);
+        logAction("Registered new client: " + client.getName());
+        return client;
+
+    }
+
+    public void unregisterClient(Client client) throws ClientNotRegisteredException {
+        if (!clients.contains(client)) {
+            throw new ClientNotRegisteredException("Error: Client is not registered");
+        }
+        clients.remove(client);
+        logAction("Unregistered client: " + client.getName());
+    }
+
+    public Instructor hireInstructor(Person person, double salaryPerHour, List<SessionType> certifications) {
+        Instructor instructor = new Instructor(person, salaryPerHour, certifications);
+        instructors.add(instructor);
+        logAction("Hired new instructor: " + instructor.getName() + " with salary per hour: " + salaryPerHour);
+        return instructor;
+    }
+
+    public Session addSession(SessionType sessionType, String date, ForumType forumType, Instructor instructor)
+            throws InstructorNotQualifiedException {
+        if (!instructor.isCertified(sessionType)) {
             throw new InstructorNotQualifiedException("Error: Instructor is not qualified to conduct this session type.");
-
         }
         Session session = new Session(sessionType, date, forum, instructor, 0, 0);
+        sessions.add(session);
+        logAction("Created new session: " + sessionType + " on " + date + " with instructor " + instructor.getName());
+        return session;
+    }
 
-        public double getSalary() {
-            return salary;
+    public void registerClientToLesson(Client client, Session session) throws DuplicateClientException, ClientNotRegisteredException {
+        if (!clients.contains(client)) {
+            throw new ClientNotRegisteredException("Error: Client is not registered");
         }
-        public void logAction(String action) {
-            actionHistroy.add(action);
+        if (!sessions.addParticipant(client)) {
+            throw new DuplicateClientException("Error: Client is already registered");
         }
-        public void printActions() {
-            for (String action : actionHistroy) {
-                System.out.println(action);
-            }
+        logAction("Registered new client: " + client.getName() + " to session: " + session);
+
+    }
+
+    public void notify(String message) {
+        for (Client client : clients) {
+            client.addNotification(message);
         }
-        public String toString() {
-            return super.toString() + " | Role: Secretary | Salary per month: " + salary;
+        logAction("A message was sent too all gym clients: " + message);
+    }
+
+    public void notify(Session session, String message) {
+        for (Client client : session.getParticipants()) {
+            client.addNotification(message);
+        }
+        logAction("A message was sent to everyone registered for session " + session + ": " + message);
+    }
+
+    public void paySalaries() {
+        double totalSalary = 0;
+        for (Instructor instructor : instructors) {
+            totalSalary += instructor.getSalaryPerHour();
+        }
+        Gym.getInstance().updateBalance(-totalSalary);
+        logAction("Salaries have been paid to all employees");
+    }
+
+    public void logAction(String action) {
+        actionHistroy.add(action);
+    }
+
+    public void printActions() {
+        for (String action : actionHistroy) {
+            System.out.println(action);
         }
     }
+
+    public String toString() {
+        return super.toString() + " | Role: Secretary | Salary per month: " + salary;
+    }
+}
 
