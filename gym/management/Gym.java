686f15e924c5e108e0e1a96b3d638d9f92d842d3
NipNaps <omri.besan@gmail.com>
2024-12-23 19:57:39 +0200

    Omri

--- gym/management/Gym.java
+++ gym/management/Gym.java
@@ -1,4 +1,41 @@
 package gym.management;
 
+import gym.customers.Person;
+
 public class Gym {
+    private static Gym instance;
+    private String name;
+    private Secretary secretary;
+    private double balance;
+
+    private Gym() {
+        this.balance = 0; // Default gym balance.
+    }
+    public static Gym getInstance() {
+        if (instance == null) {
+            instance = new Gym();
+        }
+        return instance;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Secretary getSecretary() {
+        return secretary;
+    }
+    public void setSecretary(Person perosn, double salary) {
+        this.secretary = new Secretary(perosn, salary);
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public void updateBalance(double amount) {
+        this.balance += amount;
+    }
+    public String toString() {
+        return "Gym Name " + name " | Gym Balance: " + balance + "\nSecretary: " + secretary;
+    }
 }
--- gym/management/Secretary.java
+++ gym/management/Secretary.java
@@ -1,4 +1,32 @@
 package gym.management;
 
-public class Secretary {
+import gym.customers.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+    public class Secretary extends Person {
+        private double salary;
+        private List<String> actionHistroy;
+
+        public Secretary(Person person, double salary) {
+            super(person.getName(), person.getBalance(), person.getGender(), person.getBirthdate());
+            this.salary = salary;
+            this.actionHistroy = new ArrayList<>();
+        }
+        public double getSalary() {
+            return salary;
+        }
+        public void logAction(String action) {
+            actionHistroy.add(action);
+        }
+        public void printActions() {
+            for (String action : actionHistroy) {
+                System.out.println(action);
+            }
+        }
+        public String toString() {
+            return super.toString() + " | Role: Secretary | Salary per month: " + salary;
+        }
+    }
 }
