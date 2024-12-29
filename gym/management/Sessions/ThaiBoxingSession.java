d5a3104c7cd0329279a3cba734424bb2fff9ca55
NipNaps <omri.besan@gmail.com>
2024-12-29 19:10:59 +0200

    Omri

---
+++ gym/management/Sessions/ThaiBoxingSession.java
@@ -0,0 +1,13 @@
+package gym.management.Sessions;
+
+import gym.management.Instructor;
+
+public class ThaiBoxingSession extends Session {
+    public ThaiBoxingSession(SessionType type, String dateTime, ForumType forum, Instructor instructor, int maxCapacity, int price) {
+        super(type, dateTime, forum, instructor, maxCapacity, price); {
+
+        }
+    }
+}
+
+
